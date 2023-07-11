1dd2c50274e614b43b615e228e882d6cf0c3e198
Lucio Gastellu Arrieta <104010048+lucio1907@users.noreply.github.com>
2023-07-11 12:36:54 -0300

    feat: email sended response updated

--- api/src/controllers/emails.controller.ts
+++ api/src/controllers/emails.controller.ts
@@ -7,10 +7,10 @@ const sendEmail = async (req: Request, res: Response) => {
         
         if (email === "EMAIL_SENDED") {
             await emailsServices.saveEmail(req.body);
-            return res.json({ message: "Email succesfully sended" });
+            return res.json({ message: "Email succesfully sended", status: 200 });
         }
 
-        if (email === "ERROR_TO_SEND_EMAIL") return res.status(500).json({ message: "Error to send your email" });
+        if (email === "ERROR_TO_SEND_EMAIL") return res.status(500).json({ message: "Error to send your email", status: 500 });
     } catch (error: any) {
         res.status(500).json({ error: error._message });
     }
